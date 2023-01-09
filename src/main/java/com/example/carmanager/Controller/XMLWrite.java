ae9d8a2a5be0ca6f23b1e5817b89cbb72289567c
KITT227529 <kuruczkonrad2001@gmail.com>
2023-01-09 16:00:06 +0100

    test

---
+++ src/main/java/com/example/carmanager/Controller/XMLWrite.java
@@ -0,0 +1,62 @@
+package com.example.carmanager.Controller;
+
+import com.example.carmanager.Model.Car;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+public class XMLWrite {
+
+    private static File file = new File("src/main/java/com/example/carmanager/CarsXML.xml");
+
+    public static void XMLSave (ArrayList <Car> cars) {
+
+        try {
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+            Element rootElement = document.createElement("Autók");
+            document.appendChild(rootElement);
+
+            for (Car car : cars) {
+
+                Element carElement = document.createElement("Autó");
+
+                rootElement.appendChild(carElement);
+                createChildElement(document, carElement, "Márka", car.getMake());
+                createChildElement(document, carElement, "Modell", car.getModel());
+                createChildElement(document, carElement, "Ár", car.getPrice().toString());
+                createChildElement(document, carElement, "Évjárat", car.getFirst_registration().toString());
+                createChildElement(document, carElement, "Km_futás", car.getMileage().toString());
+                createChildElement(document, carElement, "Üzemanyag", car.getFuel().toString());
+                createChildElement(document, carElement, "Kivitel", car.getBodytype().toString());
+                createChildElement(document, carElement, "Megvétel_ideje", car.getDop().toString());
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new FileOutputStream(file));
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(source, result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createChildElement (Document document, Element parent, String tagName, String text){
+        Element element = document.createElement(tagName);
+        element.setTextContent(text);
+        parent.appendChild(element);
+    }
+
+}
